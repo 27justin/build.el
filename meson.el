b2e9fd9ae011c6e4ea0e0b73d5cdf1451f75b9e8
27justin <me@justin.cx>
2024-09-17 13:54:31 +0200

    add: meson + ninja build system

---
+++ meson.el
@@ -0,0 +1,58 @@
+;;; meson.el --- Build Meson projects in Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Justin Andreas Lacoste
+
+;; Author: Justin Andreas Lacoste <me@justin.cx>
+;; URL: https://github.com/27justin/build.el
+;; Version: 0.1
+;; Package-Requires: ((emacs "?"))
+;; Keywords: compile, build-system, bazel
+
+;;; Commentary:
+
+;; This package implements a transient menu for the bazel build system.
+;; Alongside three interactive commands.
+
+;;; Requirements
+
+(require 'build-api)
+(require 'ninja)
+
+;;; Code
+
+(defun meson-project-p ()
+       (if (project-current)
+             (file-exists-p (format "%s/meson.build" (project-root (project-current))))
+             nil))
+
+(defun meson/setup (&optional directory)
+       "`meson setup' a target"
+       (interactive
+       (list (transient-arg-value "-C=" (transient-args 'meson/transient))))
+       (princ (transient-args 'meson/transient))
+       (compile (format "meson setup %s" directory)))
+
+(with-eval-after-load 'transient
+  ;; Bazel transient definition
+  (transient-define-prefix meson/transient ()
+    "Meson Build Commands"
+      :value '("build")
+    ["Meson Options\n"
+        ["Generic"
+            ("-C" "Build directory" "-C=" :prompt "Build directory: " :class transient-option :always-read t)
+        ]
+        ["Ninja Options"
+            ("-j" "Threads" "-j " :prompt "# of threads: " :class transient-option)
+            ("-n" "Dry run" "-n")
+        ]
+    ]
+    [""
+        ["Setup"
+            ("s" "Setup" meson/setup)]
+        ["Build"
+            ("b" "Build (Ninja)" ninja/build)]
+    ]))
+
+(add-to-list 'build--systems '(meson-project-p . meson/transient))
+
+(provide 'meson)
---
+++ ninja.el
@@ -0,0 +1,32 @@
+;;; meson.el --- Build Meson projects in Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Justin Andreas Lacoste
+
+;; Author: Justin Andreas Lacoste <me@justin.cx>
+;; URL: https://github.com/27justin/build.el
+;; Version: 0.1
+;; Package-Requires: ((emacs "?"))
+;; Keywords: compile, build-system, bazel
+
+;;; Commentary:
+
+;; This package implements a transient menu for the bazel build system.
+;; Alongside three interactive commands.
+
+;;; Requirements
+
+(require 'build-api)
+
+;;; Code
+
+(defun ninja/build (&optional args)
+       (interactive
+        (list (transient-args transient-current-command)))
+        (let* (
+              (arguments (string-replace "-C=" "-C " (string-join args " ")))
+              ;; Replace -C= with -C since ninja doesn't like the equal sign.
+              (command (format "ninja %s" arguments)))
+            (compile command)))
+
+(provide 'ninja)
+
